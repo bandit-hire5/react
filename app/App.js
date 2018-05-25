10be3e843dede321640f79f8c418c98507005b78
bandit <ruslannaeltok@gmail.com>
2018-05-25 13:50:37 +0300

    first commit for api

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from "react";
+
+class App extends PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Game from "../app/Game.js";
+import App from "../app/App.js";
 
 ReactDOM.render(
-    <Game />,
+    <App />,
     document.getElementById("root")
 );
